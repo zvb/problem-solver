a39919c97f06ea9451b081fbf56325104ea5d586
zvb <nkanapin@hotmail.com>
2014-11-07 21:31:23 +0600

    05 November 2014

---
+++ online-judges/timus/DS/1003/source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+const int N = 5555;
+
+int n, l[N], r[N];
+
+int main() {
+  while (1) {
+    scanf("%d", &n);
+    if (n == -1)
+      break;
+    
+  }
+  return 0;
+}
